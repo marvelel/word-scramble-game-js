9b3c19696b7a82c1a2c060dcb09cbfa44b6892d4
marvelel <sheldon.baitz@gmail.com>
2024-01-08 11:23:23 -0800

    Create word-scramble-game-js.js

---
+++ word-scramble-game-js.js
@@ -0,0 +1,5 @@
+// WORD SCRAMBLE GAME //
+/*  1. Make the code to scramble words
+    2. Words should not scramble to be the same, and should be at least some number of letters innacurate
+    3. Player should be prompted to solve the puzzle, iterate if the player is incorrect, and move on to the next word if they are correct */
+
